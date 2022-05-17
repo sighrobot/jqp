f90fe94a4b8308e45040291a7fc265c1e7296818
Abe Rubenstein <sighrobot@gmail.com>
2022-05-16 22:52:43 -0400

    improve error messaging for multi url

--- pages/api/v0.js
+++ pages/api/v0.js
@@ -21,12 +21,10 @@ const allowCors = (fn) => async (req, res) => {
   return await fn(req, res);
 };
 
-function maybeParse(text) {
-  try {
-    return JSON.parse(text);
-  } catch {
-    return Papa.parse(text, { header: true, skipEmptyLines: true }).data;
-  }
+function makeFetchErrorMsg(isMulti, idx) {
+  return `fetch failed: check that ${
+    isMulti ? (idx !== undefined ? `url[${idx}]` : 'each url') : 'url'
+  } is valid and properly encoded.`;
 }
 
 async function handler(req, res) {
@@ -45,36 +43,48 @@ async function handler(req, res) {
   }
 
   const urls = Array.isArray(url) ? url : [url];
+  const isMulti = urls.length > 1;
 
   let texts;
   try {
     const fetched = await Promise.all(urls.map((u) => fetch(u)));
+
+    fetched.forEach((f, idx) => {
+      if (f.status < 200 || f.status >= 400) {
+        return fail(makeFetchErrorMsg(isMulti, idx));
+      }
+    });
+
     texts = await Promise.all(fetched.map((f) => f.text()));
   } catch {
-    return fail(
-      'fetch failed: check that URL(s) are valid and properly encoded.',
-    );
+    return fail(makeFetchErrorMsg(isMulti));
+  }
+
+  function maybeParse(text, idx) {
+    try {
+      try {
+        return JSON.parse(text);
+      } catch {
+        return Papa.parse(text, { header: true, skipEmptyLines: true }).data;
+      }
+    } catch {
+      return fail(
+        `parse failed: check that response from ${
+          isMulti ? `url[${idx}]` : 'url'
+        } is valid JSON or CSV.`,
+      );
+    }
   }
 
   let inputJsons;
-  try {
-    inputJsons = texts.map(maybeParse);
-  } catch {
-    return fail(
-      'parse failed: check that original response is valid JSON or CSV.',
-    );
-  }
+  inputJsons = texts.map(maybeParse);
 
   let output;
   try {
-    output = await jq.run(
-      filter ?? '.',
-      inputJsons.length > 1 ? inputJsons : inputJsons[0],
-      {
-        input: 'json',
-        output: 'json',
-      },
-    );
+    output = await jq.run(filter ?? '.', isMulti ? inputJsons : inputJsons[0], {
+      input: 'json',
+      output: 'json',
+    });
   } catch {
     return fail(
       'node-jq failed: check that filter expression is valid and properly encoded.',
