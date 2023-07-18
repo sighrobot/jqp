3dd5836cacfde0fc7d9760fb197c5ed0426b07df
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2023-07-18 17:18:00 -0400

    build playground using jq-web

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const { homepage } = require('./package.json');
+/**
+ * https://github.com/fiatjaf/jq-web/issues/5#issuecomment-854231848
+ * https://github.com/vercel/next.js/issues/7755#issuecomment-812805708
+ */
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: homepage,
-        permanent: false,
-      },
-    ];
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
   },
 };
